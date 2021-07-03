b23955773592c00828163d9c163d1f3e540c7476
Edwin <86750895+Edwin22X@users.noreply.github.com>
2021-07-02 20:07:20 -0500

    Update codigo.sql

--- codigo.sql
+++ codigo.sql
@@ -25,23 +25,22 @@ CREATE TABLE Pasajero (
 ---Inserción de datos Pasajero
 ---
 ---MES-DIA-AÑO formato de la fecha
-INSERT INTO Pasajero VALUES (12564897,'Luis','Guevara','06-01-1985','masculino','peruano','luis@gmail.com',959152468);
-INSERT INTO Pasajero VALUES (15849290,'Angie','Jimenes','07-18-1998','femenino','venezolana','angie@gmail.com',949268489);
-INSERT INTO Pasajero VALUES (18364790,'Zumba','Huamaní','01-23-1990','masculino','mexicano','zumba@gmail.com',916374689);
-INSERT INTO Pasajero VALUES (12917367,'Geraldine','Salcedo','10-20-1993','femenino','chilena' ,'geraldine@gmail.com',910237890);
-INSERT INTO Pasajero VALUES (17281907,'Celeste','Pardo','12-11-1999','femenino','ecuatoriana','celeste@gmail.com',912647579);
-INSERT INTO Pasajero VALUES (12737268,'Ariana','Quevedo','12-12-1998','femenino','peruana','ariana@gmail.com',912378648);
-INSERT INTO Pasajero VALUES (11825488,'Gustavo','Mallqui','05-13-1989','masculino','peruano','gustavo@gmail.com',990736899);
-INSERT INTO Pasajero VALUES (19472579,'German','Garmendia','08-12-1990','masculino','chileno','german@gmail.com',993715167);
-INSERT INTO Pasajero VALUES (18361789,'Adriana','Sanchez','06-13-1976','femenino','argentina','adriana@gmail.com',912389756);
-INSERT INTO Pasajero VALUES (18366808,'Alejandro','Avondano','03-12-1999','masculino','brasileño','alejandro@gmail.com',916372467);
-INSERT INTO Pasajero VALUES (15728874,'Camilo','Cortés','11-11-1991','masculino','peruano','camilo@gmail.com',916371237);
-INSERT INTO Pasajero VALUES (18480752,'Diana','Lopez','10-07-1967','femenino','peruana','diana@gmail.com',995651707);
-INSERT INTO Pasajero VALUES (17469076,'Gabriel','Nieto','03-23-1999','masculino','cubano','gabriel@gmail.com',974071568);
-INSERT INTO Pasajero VALUES (18379076,'Deny','Muñoz','05-23-1998','femenino','peruana','deny@gmail.com',900086156);
-INSERT INTO Pasajero VALUES (18451808,'Cristina','Barthel','09-08-2000','femenino','boliviana','cristina@gmail.com',911186897);
---
---
+INSERT INTO Pasajero VALUES (12564897,'Luis','Guevara','06-01-1985','masculino','peruano','luis@gmail.com',959152468),
+(15849290,'Angie','Jimenes','07-18-1998','femenino','venezolana','angie@gmail.com',949268489),
+(18364790,'Zumba','Huamaní','01-23-1990','masculino','mexicano','zumba@gmail.com',916374689),
+(12917367,'Geraldine','Salcedo','10-20-1993','femenino','chilena' ,'geraldine@gmail.com',910237890),
+(17281907,'Celeste','Pardo','12-11-1999','femenino','ecuatoriana','celeste@gmail.com',912647579),
+(12737268,'Ariana','Quevedo','12-12-1998','femenino','peruana','ariana@gmail.com',912378648),
+(11825488,'Gustavo','Mallqui','05-13-1989','masculino','peruano','gustavo@gmail.com',990736899),
+(19472579,'German','Garmendia','08-12-1990','masculino','chileno','german@gmail.com',993715167),
+(18361789,'Adriana','Sanchez','06-13-1976','femenino','argentina','adriana@gmail.com',912389756),
+(18366808,'Alejandro','Avondano','03-12-1999','masculino','brasileño','alejandro@gmail.com',916372467),
+(15728874,'Camilo','Cortés','11-11-1991','masculino','peruano','camilo@gmail.com',916371237),
+(18480752,'Diana','Lopez','10-07-1967','femenino','peruana','diana@gmail.com',995651707),
+(17469076,'Gabriel','Nieto','03-23-1999','masculino','cubano','gabriel@gmail.com',974071568),
+(18379076,'Deny','Muñoz','05-23-1998','femenino','peruana','deny@gmail.com',900086156),
+(18451808,'Cristina','Barthel','09-08-2000','femenino','boliviana','cristina@gmail.com',911186897),
+(12456235,'Lucia','Carrillo','05-05-1999','femenino','venezolana','lucia@gmail.com',963258741);
 
 
 --
@@ -54,11 +53,29 @@ CREATE TABLE Pais (
 PRIMARY KEY(cod_pais),
 );
 
+
+--Inserción de datos Pais
 --
---
+
 INSERT INTO Pais VALUES
+('BRA','Brasil'),
+('BOL','Bolivia'),
+('CHL','Chile'),
+('COL','Colombia'),
+('CRI','Costa Rica'),
+('ECU','Ecuador'),
+('GTM','Guatemala'),
+('ITA','Italia'),
+('MEX','Mexico'),
+('NIC','Nicaragua'),
+('PAN','Panamá'),
+('URY','Uruguay'),
 ('USA','Estado Unidos'),
-;
+('ESP','España'),
+('PER','Perú'),
+('DEU','Alemania'),
+('VEN','Venezuela');
+
 
 --
 -- creación tabla ESTADO
@@ -70,6 +87,16 @@ CREATE TABLE Estado (
 PRIMARY KEY(cod_estado),
 );
 
+
+--Inserción de datos Estado
+--
+
+INSERT INTO Estado VALUES 
+('C','Cancelled'), 
+('D','Delayed'), 
+('O','On Time');
+
+
 --
 -- creación tabla TIPO VUELO
 --
@@ -80,6 +107,15 @@ CREATE TABLE Tipo_vuelo (
 PRIMARY KEY(cod_tipo),
 );
 
+
+--Inserción datos Tipo_vuelo
+--
+
+INSERT INTO Tipo_vuelo VALUES
+('SI','Solo Ida'),
+('IV','Ida y Vuelta');
+
+
 --
 -- creación tabla CLASE VUELO
 --
@@ -89,6 +125,15 @@ CREATE TABLE Clase_vuelo (
   clase VARCHAR(50) NOT NULL, 
 PRIMARY KEY(cod_clase),
 );
+
+
+--Inserción datos Clase_vuelo
+--
+
+INSERT INTO Clase_vuelo VALUES
+('FC','First Class'),
+('BC','Business Class'),
+('TC','Tourist Class');
 
 
 --
@@ -103,6 +148,30 @@ PRIMARY KEY(cod_aeropuerto),
 CONSTRAINT fk_pais FOREIGN KEY(cod_pais) REFERENCES Pais(cod_pais),
 );
 
+
+--Inserción de datos Aeropuerto
+--
+
+INSERT INTO Aeropuerto VALUES
+('AANF','Aeropuerto Cerro Moreno','CHL'),
+('ABCN','Aeropuerto de Barcelona-El Prat','ESP'),
+('ABOG','Aeropuerto Internacional El Dorado','COL'),
+('AGRU','Aeropuerto Internacional de São Paulo-Guarulhos','BRA'),
+('AVVI','Aeropuerto Internacional Viru Viru','BOL'),
+('ASJO','Aeropuerto Internacional Juan Santamaría','CRI'),
+('AMEC','Aeropuerto de Manta','ECU'),
+('APBR','Aeropuerto de Puerto Barrios','GTM'),
+('ANAP','Aeropuerto de Nápoles-Capodichino','ITA'),
+('AGDL','Aeropuerto Internacional de Guadalajara','MEX'),
+('AMGA','Aeropuerto Internacional Augusto C. Sandino','NIC'),
+('APTY','Aeropuerto Internacional de Tocumen','PAN'),
+('AMVD','Aeropuerto Internacional de Carrasco','URY'),
+('ALAX','Aeropuerto Internacional de Los Ángeles','USA'),
+('ALIM','Aeropuerto Internacional Jorge Chávez','PER'),
+('AHAM','Aeropuerto de Hamburgo','DEU'),
+('ACCS','Aeropuerto Internacional de Maiquetía Simón Bolívar','VEN');
+
+
 --
 -- creación tabla AEROLINEA
 --
@@ -114,6 +183,29 @@ aeropuerto_principal VARCHAR(4) NOT NULL,
 PRIMARY KEY(cod_aerolinea),
 CONSTRAINT fk_aeropuerto FOREIGN KEY(aeropuerto_principal) REFERENCES Aeropuerto(cod_aeropuerto),
 );
+
+--Inserción datos Aerolinea
+--
+
+INSERT INTO Aerolinea VALUES
+('AAU','American Airlines','ALAX'),
+('APR','Avianca','ALIM'),
+('IES','Interjet','ABCN'),
+('SAD','Sky Airline','AHAM'),
+('BAB','Boliviana de Aviacion','AVVI'),
+('VCC','VivaColombia','ABOG'),
+('SCR','Star','ASJO'),
+('QAC','Qatar Airways','AANF'),
+('AIT','Alitalia','ANAP'),
+('JBG','JetBlue','APBR'),
+('LAB','LATAM Airlines Brasil','AGRU'),
+('AFU','Aeroflot','AMVD'),
+('AEV','Air Europa','ACCS'),
+('SAE','Spirit Airlines','AMEC'),
+('AMX','Aeromexico','AGDL'),
+('KNI','KLM','AMGA'),
+('XAP','Xiamen Airlines','APTY');
+
 
 --
 -- creación tabla AVIONES
@@ -127,6 +219,28 @@ capacidad INT NOT NULL,
 PRIMARY KEY(cod_avion),
 CONSTRAINT fk_aerolinea FOREIGN KEY(cod_aerolinea) REFERENCES Aerolinea(cod_aerolinea),
 );
+
+--Inserción de datos Aviones
+--
+
+INSERT INTO Aviones VALUES
+('A32X','Airbus A330-200','XAP',264),
+('A77K','Boeing 737-700','KNI',128),
+('B78A','Boeing 737-800','AMX',170),
+('B7MS','Boeing 737 MAX','SAE',170),
+('E1AE','Embraer 190','AEV',96),
+('AA8A','Airbus A380-800','AFU',153),
+('AA6L','Airbus A340-600','LAB',280),
+('B73J','Boeing 777-300','JBG',150),
+('B74A','Boeing 747-400','AIT',126),
+('B7IQ','Boeing 747-8 Intercontinental','QAC',267),
+('M23S','MS-21-300','SCR',211),
+('C9VC','C919','VCC',150),
+('ABX8','Airbus Beluga XL','BAB',159),
+('AA3S','Airbus A319','SAD',163),
+('A32I','Airbus 330-200','IES',100),
+('B7MA','Boeing 737 MAX 8','APR',189),
+('B78U','Boeing 780 - 800','AAU',160);
 
 --
 -- creación tabla VUELOS
@@ -145,6 +259,29 @@ CONSTRAINT fk_paisO FOREIGN KEY(pais_origen) REFERENCES Pais(cod_pais),
 CONSTRAINT fk_paisD FOREIGN KEY(pais_destino) REFERENCES Pais(cod_pais),
 );
 
+
+--Inserción datos Vuelos
+--
+
+INSERT INTO Vuelos VALUES
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+('AAAA','AAAA','AAA','AAA','2021-99-99 99:99:00','2021-99-99 99:99:00');
+
 --
 -- creación tabla SALIDAS
 --
@@ -161,6 +298,28 @@ CONSTRAINT fk_vueloS FOREIGN KEY(cod_vuelo) REFERENCES Vuelos(cod_vuelo),
 CONSTRAINT fk_destinoS FOREIGN KEY(destino) REFERENCES Pais(cod_pais),
 CONSTRAINT fk_estadoS FOREIGN KEY(cod_estado) REFERENCES Estado(cod_estado),
 );
+
+--Insercion de datos Salidad
+--
+
+INSERT INTO Salidas VALUES
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
+('AAA','AAAA','AAA','99-99-99','A','AAA'),
 
 --
 -- creación tabla RESERVACION
